d4b78330104f3cbad36583a98938fc516b6e774b
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-08 09:31:36 -0500

    commit tree 029e2069916dae384ce46db2e32cdde40c5b44b7

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
-import { connect } from 'react-redux';
-import { addItem } from  './actions/items';
+import React, { Component } from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import { addItem } from "./actions/items";
 
 class App extends Component {
-
   handleOnClick() {
-    this.props.store.dispatch(addItem());
+    this.props.addItem();
   }
 
   render() {
+    debugger;
     return (
       <div className="App">
-        <button onClick={(event) => this.handleOnClick(event)}>
-          Click
-          </button>
+        <button onClick={(event) => this.handleOnClick(event)}>Click</button>
         <p>{this.props.items.length}</p>
       </div>
     );
   }
-};
-
-const mapStateToProps = (state) => {
-  return {
-    items: state.items
-  };
-};
-
-export default connect(mapStateToProps)(App);
+}
+export default connect((state) => ({ items: state.items }))(App);
